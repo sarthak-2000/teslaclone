43214946d3625ea7e8aa8baa7fb57b826fe05ef7
sarthak-2000 <sarthakjariwala@outlook.com>
2021-12-24 12:34:14 -0800

    New Updated Files

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, View } from 'react-native';
+import React from 'react';
+import CarsList from './components/carslist';
+import Header from './components/header';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Header/>
+    <CarsList/>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +21,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+ 
+
 });
---
+++ components/stylebutton/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {View, Text, Pressable} from 'react-native';
+// import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+import styles from './styles';
+
+
+
+const StyledButton  = (props) => {
+    
+ //   const type = props.type;    
+    //const content = props.content;
+  //  const onPress = props.onPress;
+    const{type, content, onPress} = props;
+    const backgroundColor = type === 'primary' ? 'black': 'white';
+    const textColor= type === 'primary' ? 'white': 'black';
+        
+  
+    return (
+        <View style={styles.container}>
+            <Pressable
+            style={[styles.button, {backgroundColor: backgroundColor}]}
+            onPress={() => onPress()
+            }>
+            <Text style = {[styles.text, {color: textColor }]}>{content}</Text>
+
+            </Pressable>
+        </View>
+    
+    
+    );
+
+    };
+export default StyledButton;
